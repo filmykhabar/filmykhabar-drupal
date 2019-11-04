cc7c6c41f4214cb1c9b08eab7f4d711121ef3296
Chhabi Pachabhaiya <c.pachabhaiya@gmail.com>
2019-11-04 00:07:04 -0600

    Add a new custom module 'fkm'

---
+++ web/modules/custom/fkm/src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\fkm\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class DefaultController.
+ */
+class DefaultController extends ControllerBase {
+
+  /**
+   * Home.
+   *
+   * @return array
+   *   Return empty array.
+   */
+  public function home() {
+    return [];
+  }
+
+}
