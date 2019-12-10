fb2aa04ec38464351ab8eb4a71bcf9ad51a9219f
Chhabi Pachabhaiya <c.pachabhaiya@gmail.com>
2019-12-09 23:49:17 -0600

    Migrate photo gallery

---
+++ web/modules/custom/migrate_filmykhabar/src/Plugin/migrate/source/ContentPhotos.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Drupal\migrate_filmykhabar\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+use Drupal\Core\Database\Database;
+
+/**
+ * Provides a 'ContentPhotos' migrate source.
+ *
+ * @MigrateSource(
+ *   id = "content_photos"
+ * )
+ */
+class ContentPhotos extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $c_type = 151;
+    $parent_id = 11;
+    $status = 402;
+
+    $fields_category = [
+      'category_id',
+      'thumbnail_name',
+      'thumbnail_type',
+      'date_created',
+      'date_modified',
+    ];
+
+    $fields_category_detail = [
+      'category_detail_id',
+      'category_detail_code',
+      'title',
+      'description',
+    ];
+
+    $subquery = $this->select('Category', 'c');
+    $subquery
+      ->fields('c', array('category_id'))
+      ->condition('c.parent_id', $parent_id);
+
+    $query = $this->select('Category', 'c');
+    $query->join('CategoryDetail', 'cd', 'c.category_id = cd.category_id AND cd.language_id = :language_id', array(':language_id' => 1));
+    $query
+      ->fields('c', $fields_category)
+      ->fields('cd', $fields_category_detail)
+      ->condition('c.type', $c_type)
+      ->condition('c.status', $status)
+      ->condition('c.parent_id', $subquery, 'IN')
+      ->orderBy('c.date_created', 'ASC');
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'category_id' => $this->t('Category ID'),
+      'thumbnail_name' => $this->t('Thumbnail name'),
+      'thumbnail_type' => $this->t('Thumbnail type'),
+      'date_created' => $this->t('Date created'),
+      'date_modified' => $this->t('Date modified'),
+      'category_detail_id' => $this->t('Content detail ID'),
+      'category_detail_code' => $this->t('Content detail code'),
+      'title' => $this->t('Title'),
+      'description' => $this->t('Description'),
+      'path_alias' => $this->t('Path alias'),
+      'image' => $this->t('Promo image'),
+      'image_destination' => $this->t('Promo image destination'),
+      'title_decoded' => $this->t('Decoded title'),
+      'picture_ids' => $this->t('Picture IDs'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'category_id' => [
+        'type' => 'integer',
+        'alias' => 'c',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $category_id = $row->getSourceProperty('category_id');
+    $category_detail_id = $row->getSourceProperty('category_detail_id');
+
+    // Teaser image
+    $thumbnail_name = $row->getSourceProperty('thumbnail_name');
+    $thumbnail_type = $row->getSourceProperty('thumbnail_type');
+    $image = 'http://www.filmykhabar.com/data/picture/o/' . md5($thumbnail_name) . '@filmykhabar.com.' . $thumbnail_type;
+    $row->setSourceProperty('image', $image);
+
+    // Teaser image destination
+    $image_destination = 'public://images/' . date('Y-m', strtotime($row->getSourceProperty('date_created'))) . '/' . $category_id . '.' . $thumbnail_type;
+    $row->setSourceProperty('image_destination', $image_destination);
+
+    // Decoded title - for image alt/title
+    $title_decoded = str_replace(array('&quot;', '&#039;', '&nbsp;', "  "), array('"', "'", " ", " "), $row->getSourceProperty('title'));
+    $title_decoded = html_entity_decode($title_decoded);
+    $title_decoded = trim($title_decoded);
+    $row->setSourceProperty('title_decoded', $title_decoded);
+
+    // Path alias
+    $path_alias = '/pictures/' . $category_detail_id;
+    $row->setSourceProperty('path_alias', $path_alias);
+
+    // Pictures
+    $query = $this->select('Picture', 'p');
+    $query
+      ->fields('p', array('picture_id'))
+      ->condition('p.category_id', $category_id)
+      ->condition('p.status', 402)
+      ->orderBy('p.sequence', 'ASC');
+    $result = $query->execute()->fetchAll();
+    $picture_ids = [];
+    foreach ($result as $val) {
+      $val = (array) $val;
+      $picture_ids[] = $val['picture_id'];
+    }
+    $row->setSourceProperty('picture_ids', $picture_ids);
+
+    // Get paragraphs mappings.
+    $drupalDb = Database::getConnection('default', 'default');
+    $paragraphs = [];
+    $query_mmppi = $drupalDb->select('migrate_map_pictures_paragraphs_item', 'mmppi')
+      ->fields('mmppi', ['destid1', 'destid2'])
+      ->condition('mmppi.sourceid1', $picture_ids, 'IN');
+    $results_mmppi = $query_mmppi->execute()->fetchAll();
+    if (!empty($results_mmppi)) {
+      foreach ($results_mmppi as $result_mmppi) {
+        $paragraphs[] = [
+          'target_id' => $result_mmppi->destid1,
+          'target_revision_id' => $result_mmppi->destid2,
+        ];
+      }
+    }
+    $row->setSourceProperty('field_photo_gallery', $paragraphs);
+
+    return parent::prepareRow($row);
+  }
+}
---
+++ web/modules/custom/migrate_filmykhabar/src/Plugin/migrate/source/Pictures.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Drupal\migrate_filmykhabar\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Provides a 'Pictures' migrate source.
+ *
+ * @MigrateSource(
+ *   id = "pictures"
+ * )
+ */
+class Pictures extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $c_type = 151; // table=OptionItem, option_item_id=151, option_item_code=picture
+    $parent_id = 11; // table=Category, category_id=11 (category_detail_code=picture-gallery)
+    $status = 402; // table=OptionItem, option_item_id=402, option_item_code=active
+
+    $subsubquery = $this->select('Category', 'c');
+    $subsubquery
+      ->fields('c', array('category_id'))
+      ->condition('c.parent_id', $parent_id);
+
+    $subquery = $this->select('Category', 'c');
+    $subquery
+      ->fields('c', array('category_id'))
+      ->condition('c.type', $c_type)
+      ->condition('c.status', $status)
+      ->condition('c.parent_id', $subsubquery, 'IN');
+
+    $query = $this->select('Picture', 'p');
+    $query->join('PictureDetail', 'pd', 'pd.picture_id = p.picture_id AND pd.language_id = :language_id', array(':language_id' => 1));
+    $query->join('Category', 'c', 'c.category_id = p.category_id');
+    $query->join('CategoryDetail', 'cd', 'cd.category_id = p.category_id AND cd.language_id = :language_id', array(':language_id' => 1));
+    $query
+      ->fields('p', array('picture_id', 'category_id', 'file_name', 'file_type', 'date_created', 'date_modified'))
+      ->fields('pd', array('picture_detail_code', 'photographer', 'title', 'excerpt'))
+      ->fields('c', array('parent_id'))
+      ->fields('cd', array('category_detail_code'))
+      ->condition('p.category_id', $subquery, 'IN')
+      ->condition('p.status', $status)
+      ->orderBy('p.date_created', 'ASC');
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'picture_id' => $this->t('Picture ID'),
+      'category_id' => $this->t('Category ID'),
+      'file_name' => $this->t('File name'),
+      'file_type' => $this->t('File type'),
+      'date_created' => $this->t('Date created'),
+      'date_modified' => $this->t('Date modified'),
+      'picture_detail_code' => $this->t('Picture detail code'),
+      'title' => $this->t('Title'),
+      'excerpt' => $this->t('Excerpt'),
+      'photographer' => $this->t('Photographer'),
+      'path_alias' => $this->t('Path alias'),
+      'image' => $this->t('Image'),
+      'image_destination' => $this->t('Image destination'),
+      'title_decoded' => $this->t('Decoded title'),
+      'parent_id' => $this->t('Parent ID'),
+      'category_detail_code' => $this->t('Category detail code'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'picture_id' => [
+        'type' => 'integer',
+        'alias' => 'p',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $picture_id = $row->getSourceProperty('picture_id');
+    $category_detail_code = $row->getSourceProperty('category_detail_code');
+
+    // Teaser image
+    $file_name = $row->getSourceProperty('file_name');
+    $file_type = $row->getSourceProperty('file_type');
+    $file_name_type = md5($file_name) . '@filmykhabar.com.' . $file_type;
+    $image = 'http://www.filmykhabar.com/data/picture/o/' . $file_name_type;
+    $row->setSourceProperty('image', $image);
+
+    // Teaser image destination
+    $image_destination = 'public://images/' . date('Y-m', strtotime($row->getSourceProperty('date_created'))) . '/' . $file_name_type;
+    $row->setSourceProperty('image_destination', $image_destination);
+
+    // Decoded title - for image alt/title
+    $title_decoded = str_replace(array('&quot;', '&#039;', '&nbsp;', "  "), array('"', "'", " ", " "), $row->getSourceProperty('title'));
+    $title_decoded = html_entity_decode($title_decoded);
+    $title_decoded = trim($title_decoded);
+    $row->setSourceProperty('title_decoded', $title_decoded);
+
+    // Path alias
+    $path_alias = '/pictures/' . $category_detail_code . '/' . $picture_id;
+    $row->setSourceProperty('path_alias', $path_alias);
+
+    return parent::prepareRow($row);
+  }
+}
