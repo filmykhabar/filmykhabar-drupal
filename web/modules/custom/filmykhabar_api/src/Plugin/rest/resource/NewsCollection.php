8e26e570e1d928edbdcc34fc517e948dd5ca18e7
Chhabi Pachabhaiya <c.pachabhaiya@gmail.com>
2021-06-19 15:47:02 -0500

    News collection resource

--- web/modules/custom/filmykhabar_api/src/Plugin/rest/resource/NewsCollection.php
+++ web/modules/custom/filmykhabar_api/src/Plugin/rest/resource/NewsCollection.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\filmykhabar_api\Plugin\rest\resource;
 
-use Drupal\rest\ModifiedResourceResponse;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -19,27 +18,35 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
  *   }
  * )
  */
-class NewsCollection extends ResourceBase {
-
-  /**
-   * A current user instance.
-   *
-   * @var \Drupal\Core\Session\AccountProxyInterface
-   */
-  protected $currentUser;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
-    $instance->logger = $container->get('logger.factory')->get('filmykhabar_api');
-    $instance->currentUser = $container->get('current_user');
-    return $instance;
-  }
+class NewsCollection extends ResourceBase
+{
 
     /**
-     * Responds to GET requests.
+     * A current user instance.
+     *
+     * @var \Drupal\Core\Session\AccountProxyInterface
+     */
+    protected $currentUser;
+
+    /**
+     * The database connection.
+     */
+    protected $database;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+    {
+        $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+        $instance->logger = $container->get('logger.factory')->get('filmykhabar_api');
+        $instance->currentUser = $container->get('current_user');
+        $instance->database = $container->get('database');
+        return $instance;
+    }
+
+    /**
+     * Gets a list of news.
      *
      * @return \Drupal\rest\ResourceResponse
      *   The HTTP response object.
@@ -47,15 +54,34 @@ class NewsCollection extends ResourceBase {
      * @throws \Symfony\Component\HttpKernel\Exception\HttpException
      *   Throws exception expected.
      */
-    public function get() {
-
+    public function get()
+    {
         // You must to implement the logic of your REST Resource here.
         // Use current user after pass authentication to validate access.
         if (!$this->currentUser->hasPermission('access content')) {
             throw new AccessDeniedHttpException();
         }
 
-        return new ResourceResponse('hello world', 200);
-    }
+        $responseData = [];
 
+        try {
+            $query = $this->database->select('node_field_data', 'nfd');
+            $query
+                ->condition('nfd.status', 1)
+                ->fields('nfd', ['nid', 'title', 'created'])
+                ->range(0, 11)
+                ->orderBy('nfd.created', 'DESC');
+
+            $responseData = $query->execute()->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+        }
+
+        $response = new ResourceResponse($responseData, 200);
+        // In order to generate fresh result every time (without clearing
+        // the cache), you need to invalidate the cache.
+        $response->addCacheableDependency($responseData);
+
+        return $response;
+    }
 }
